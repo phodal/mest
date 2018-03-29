68ebd70b4ca87eafbf6cfaf41709c0cd033c0839
Phodal HUANG <h@phodal.com>
2018-03-29 08:49:48 +0800

    [T] make cli works

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+import { default as Mest } from './mest'
+
+const colors = require('colors')
+const program = require('commander')
+
+const version = require('../../package.json').version
+
+function testSchema(inputFile: string) {
+  let mest = new Mest({
+    file: inputFile
+  })
+  mest.load()
+}
+
+program
+  .version(version)
+  .option('-i, inputFile <inputFile>', 'Compare API', testSchema)
+  .parse(process.argv)
+
+if (!process.argv.slice(2).length || !process.argv.length) {
+  program.outputHelp(colors.green)
+}
