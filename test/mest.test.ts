e72c502f7ae9d5330b0603149d869f8d10a4330a
Phodal HUANG <h@phodal.com>
2018-04-09 20:03:45 +0800

    [T] test simple get reponse api

--- test/mest.test.ts
+++ test/mest.test.ts
@@ -26,3 +26,32 @@ describe('load file test', () => {
     // expect(console.log).toBeCalled()
   })
 })
+
+describe('local compare test', () => {
+  it('basic compare', () => {
+    jest.spyOn(global.console, 'log')
+    let mest = new Mest({
+      file: 'data/url.csv'
+    })
+
+    mest.localCompareInterface('mock/IUser.ts', {
+      login: 'phodal',
+      id: 472311,
+      type: 'User',
+      site_admin: false,
+      name: 'Phodal Huang',
+      company: '@ThoughtWorksInc',
+      blog: 'https://www.phodal.com/',
+      location: 'Shenzhen, China',
+      email: null,
+      hireable: true,
+      bio: '待我代码编成，娶你为妻可好\r\n',
+      public_repos: 254,
+      public_gists: 12,
+      followers: 11152,
+      following: 15,
+      created_at: '2010-11-08T11:46:51Z',
+      updated_at: '2018-03-25T14:31:00Z'
+    })
+  })
+})
